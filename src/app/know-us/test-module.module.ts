ae236564690ec12f2a1441eb3c6840d392ecb15c
ElsayedAhmed0 <elsayed.ahmed7@outlook.com>
2021-09-19 16:21:53 +0200

    ad

---
+++ src/app/know-us/test-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KnowUsComponent } from './know-us.component';
+
+
+
+@NgModule({
+  declarations: [KnowUsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TestModuleModule { }
